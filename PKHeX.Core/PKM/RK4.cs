f00094a666f232098ad519a0a893133af1775790
Kurt <kwsch@users.noreply.github.com>
2026-07-15 22:23:01 -0500

    RK4: split handler char count from OT
    Needed because RK4 reserves 10char for the handler.

--- PKHeX.Core/PKM/RK4.cs
+++ PKHeX.Core/PKM/RK4.cs
@@ -317,7 +317,9 @@ public sealed class RK4 : G4PKM
     public ushort HandlingTrainerTID { get => ReadUInt16LittleEndian(Data[0x8C..]); set => WriteUInt16LittleEndian(Data[0x8C..], value); }
     public ushort HandlingTrainerSID { get => ReadUInt16LittleEndian(Data[0x8E..]); set => WriteUInt16LittleEndian(Data[0x8E..], value); }
 
-    public override Span<byte> HandlingTrainerTrash => Data.Slice(0x90, 0x10);
+    public override Span<byte> HandlingTrainerTrash => Data.Slice(0x90, 20);
+    public override int TrashCharCountHandler => 10;
+
     public override string HandlingTrainerName
     {
         get => StringConverter4.GetString(HandlingTrainerTrash);
